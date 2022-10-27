a3be437b2710bf6e66c2c9b8648494f7fb577009
Ash Malmsten <32045016+skipday@users.noreply.github.com>
2022-10-27 13:50:49 +0200

    dev version

--- src/index.js
+++ src/index.js
@@ -1,60 +1,46 @@
-import CheatSheet from './cheatsheet.js'
-import { arbitrarySupportedClasses } from './cheatsheet.js';
-import juice from 'juice';
-import twClasses from './allTailwindClasses.json' assert {type: 'json'}
+import twClasses from './regexedclasses.json' assert {type: 'json'}
+
+//key = key from tailwind object. str = each string from class=""
+const objToCssString = (key, str) => {
+    str = str.insert(0, '.')
+    //custom value ex. max-w-[400px]
+    if(typeof str !== 'string') {
+        let tempObj = twClasses[str[0]]
+        Object.keys(tempObj).forEach(key => tempObj[key] = tempObj[key] = str[1].replace(/(\[|\])/g, ''))
+        return JSON.stringify(twClasses[key]).replace(/(,|})/g, ';').replace(/("|{)/g, '')
+    }
+    //more than one key. TODO: I think this will break with multiple keys & values with , in them. Like multiple box shadow properties? ignored for now.
+    else if (Object.keys(twClasses[key]).length >= 2) return JSON.stringify(twClasses[key]).replace(/(,|})/g, ';').replace(/("|{)/g, '')
+    else  return JSON.stringify(twClasses[key]).replace(/(})/g, ';').replace(/("|{)/g, '')
+}
 
 const convertCss = (classArr) => {
-    //for each key in twclasses, see if it matches the current string. If it does, replace the string with the value of the key
-    classArr = classArr.split(' ')
+    classArr = classArr.replace(/\w[a-zA-Z0-9-]+(?<VALUE>\[.+?\])/g, (match, value) => Array.from([match.replace(value, '4'), value])).split(' ')
+    .map(e => (e.match(/,/g)) ? e.split(',') : e)
     let result = []
     Object.keys(twClasses).forEach(key => {
-        //TODO: with an input mb-[24px], look for the first mb, get the key value pair and replace the value with the content inside []
         classArr.forEach(str => {
-            if(key !== str) return
-            let tempres = JSON.stringify(twClasses[key]).replace(/"/g, '').replace('{', '').replace('}', ';')
-            result.push(tempres)
+            if(key !== '.' + str && key !== '.' + str[0]) return
+            result.push(objToCssString(key, str))
         })
     })
     return result.join(' ')
 }
 
-const main = (html) => {
-    //given an input html string, match tag for tag with class="" and replace the tag with convertCss + juice result
-    var regex = new RegExp('\<(?<TAG>[a-z]{1,15}) (?:(?:class|className)="(?<CLASS>.+?)")?', 'g')
-    html = html.replace(regex, (match,tag,tagMatch) => {
-        //extract and convert classnames
-        let tempCss = convertCss(tagMatch)
-        const toJuice = `${tag} { ${tempCss} }`
-        
-        let testStr = '<div></div>'
-        console.log(juice.inlineContent(testStr, toJuice))
-    })
-    // console.log(html)
-    /*
-    let css = html.match(/class="(.*)"/g).join().replace('class="', '').replace('"', '')
-    var tempreg = /\<(?<TAG>[a-z]{1,15}) (?:(?:class|className)=(?<CLASS>(?:'.+?')|".+?"))?/g
-    var regex = new RegExp('\<(?<TAG>[a-z]{1,15}) (?:(?:class|className)="(?<CLASS>.+?)")?', 'g')
-    let match = Array.from(html.matchAll(regex), (m) => [m.groups.TAG, m.groups.CLASS])
-    match.forEach(arr => {
-        convertCss(match[0][1])
-    })
-    */
-    //spit out the 
+String.prototype.insert = function (index, string) {
+    var ind = index < 0 ? this.length + index  :  index;
+    return  this.substring(0, ind) + string + this.substr(ind);
+};
 
-    /*
-    css = `${match[0]} { ${match[1]} }`
-    console.log(css)*/
-    /*
-    css = convertToCSS(css)
-    css = css.split(' ')
-    css = ['{', ...css, '}']
-    html = html.replace(/ class="(.*)"/g, '')
-    //css to inline styling
-    console.log(css)
-    //append the html tag to the start with brackets
-    let test2 = 'p { color: green }'
-    let test = juice.inlineContent(html, test2)
-    console.log(test)*/
+const main = (html) => {
+    var regex = new RegExp(String.raw`<(?<TAG>.+?) (?:(?:class|className)=(?<CLASS>(?:".+?"))).*?>`, 'g')
+    html = html.replace(regex, (match,tag,tagMatch) => {
+        tagMatch = tagMatch.replace(/"/g, '')
+        let tempCss = convertCss(tagMatch)
+        const styleTag = ` style="${tempCss}"`
+        return match.insert(-1, styleTag)
+    })
+    return html
 }
 
 export default main
--- src/test.js
+++ src/test.js
@@ -1,5 +1,11 @@
 import convert from './index.js'
 
-let test = 'drop-shadow flex'
+let testHtml = String.raw`<article class="container bg-white shadow-2xl rounded-2xl p-5">
+<h1 class="font-bold text-yellow-500">Mon blog en ligne</h1>
+<p class="font-light text-gray-500 hover:font-bold">Aujoud'hui dans mon blog nous allons parler de fruits et légumes.....</p>
+<h6 class="text-sm text-gray-300 mb-5">Publiée le 08/10/2022</h6>
+<a href="#" class="rounded-lg py-2 px-4 text-center text-white bg-yellow-400 hover:bg-yellow-500">En savoir plus</a>
+</article>`
 
-let res = convert('<div class="shadow-md text-blue-500 flex gap-1 mb-2"><span className="text-blue-500 bg-green-200">hello there</span></div>')
+let res = convert(testHtml)
+console.log(res)
