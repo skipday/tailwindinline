60916ada3e095acffdca3aba4b4f5455141288ed
Ash Malmsten <32045016+skipday@users.noreply.github.com>
2022-10-30 01:14:24 +0200

    Readability improvements

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-npm install tailwindinline
-import convert from 'tailwindinline'
+* npm install tailwindinline
+* import convert from 'tailwindinline'
 
 # Tailwindinline
-Appends a ==style attribute== with computed tailwind classes to the end of each html tag with tailwind classes.
+(Tries to) Append a style attribute with computed tailwind classes to the end of each html tag that contained them.
 
 # Planned additions
 - [x] remove unneeded styles from classList.json (hover, focus)
-- [ ] fix syntax of some classnames in classList.json (ex: placeholder:moz-[...])
+- [x] fix syntax of some classnames in classList.json (ex: placeholder:moz-[...])
 - [ ] add all tailwind auto-added classes
 - [ ] custom tailwind classes
 - [ ] implement ts regex file
--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 import twClasses from './classList.json' assert {type: 'json' }
+const classList = new Map(Object.entries(twClasses));
 //key = key from tailwind object. str = each string from class=""
-const objToCssString = (key, str) => {
-    if(typeof str !== 'string') { //{ [ gap: 1rem, row-gap: 1rem ] } => gap: 1rem; row-gap: 1rem;
-        str[0] = str[0].insert(0, '.')
-        let tempObj = twClasses[str[0]]
-        Object.keys(tempObj).forEach(key => tempObj[key] = tempObj[key] = str[1].replace(/(\[|\])/g, ''))
-        return JSON.stringify(twClasses[key]).replace(/(,|})/g, ';').replace(/("|{)/g, '')
-    }
-    //more than one key. TODO: I think this will break with multiple keys & values with , in them. Like multiple box shadow properties? ignored for now.
-    else if (Object.keys(twClasses[key]).length >= 2) {return JSON.stringify(twClasses[key]).replace(/(,|})/g, ';').replace(/("|{)/g, '')}
-    else  return JSON.stringify(twClasses[key]).replace(/(})/g, ';').replace(/("|{)/g, '')
+const objToCssString = (key, customValue = false, tag) => {
+    if(customValue) Object.entries(key)[0][1] = customValue
+    return Object.entries(key)[0].join(': ')
 }
 
 const convertCss = (classArr) => {
-    classArr = classArr.replace(/\w[a-zA-Z0-9-]+(?<VALUE>\[.+?\])/g, (match, value) => Array.from([match.replace(value, '4'), value])).split(' ')
+    classArr = classArr.replace(/\w[a-zA-Z0-9-]+(?<VALUE>\[(.+?)\])/g, (match, value) => Array.from([match.replace(value, '4'), value])).split(' ')
     .map(e => (e.match(/,/g)) ? e.split(',') : e)
     if(!classArr.includes('box-content') && !classArr.includes('box-border')) classArr.push('box-border')
     let result = []
-    Object.keys(twClasses).forEach(key => {
-        classArr.forEach(str => {
-            if(key !== '.' + str && key !== '.' + str[0]) return
-            result.push(objToCssString(key, str))
-        })
+    classArr.forEach(classN => {
+        if(typeof classN !== 'string') {
+            classN[1] = classN[1].replace(/\[|\]/g, '')
+            classList.has(`.${classN[0]}`) ? result.push(objToCssString(classList.get(`.${classN[0]}`),classN[1])) : ''
+        } else classList.has(`.${classN}`) ? result.push(objToCssString(classList.get(`.${classN}`))) : ''
     })
-    return result.join(' ')
+    return result.join('; ')
 }
 
 String.prototype.insert = function (index, string) {
@@ -34,7 +28,7 @@ String.prototype.insert = function (index, string) {
 const main = (html) => {
     if(!html) return
     var regex = new RegExp(String.raw`<(?!\/).+?>`, 'g')
-    html = html.replace(regex, (match,tag,tagMatch) => {
+    return html.replace(regex, (match) => {
         let styles = ''
         match = match.replace(/(?:(?:class|className)=(?<CLASS>(?:".+?"))).*?/g, (m,innerClasses) => {
             innerClasses = innerClasses.replace(/"|'/g, '')
@@ -47,7 +41,6 @@ const main = (html) => {
         if(match.match(/(?:style=".+?")/g)) return match.replace(/(?:style=".+?")/g, (e,m,x) => e.insert(-1, '; ' + styles))
         else return match.insert(-1, styleTag )
     })
-    return html
 }
 
 export default main
