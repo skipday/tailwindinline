5208e609f30cb4af3c6863e1d574888c216c4ef5
Ash Malmsten <32045016+skipday@users.noreply.github.com>
2024-02-19 11:38:07 +0100

    lock inner config, extend outer config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const { dirname } = require('path');
+const appDir = dirname(require.main.filename);
+
+const outerConfig = require(`${appDir}/tailwind.config.js`)
+const outerExtend = outerConfig?.theme?.extend || {}
+
 export default {
   content: ["**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      ...outerExtend
+    },
   },
   plugins: [],
 }
